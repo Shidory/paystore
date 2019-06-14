92b1075ccdce03d57d2b381ccf40ddb464233a09
Simon MWEPU <simonmwepu@gmail.com>
2019-06-14 18:08:09 +0200

    [ADD] rendering

--- paystore/views.py
+++ paystore/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
+    if request.user.is_authenticated():
+        username = "Shidory"
+        context = {"username": request.user}
+    else:
+        context = {"username": request.user}
+        
     template = 'products/home.html'
     context = locals()
     return render(request, template, context)
