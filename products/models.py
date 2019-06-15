adf5eb23154f203a91dc95cf10f307b72a3863d0
Simon MWEPU <simonmwepu@gmail.com>
2019-06-15 14:38:59 +0200

    [ADD] ProductImage class

--- products/models.py
+++ products/models.py
@@ -4,6 +4,8 @@ class Product(models.Model):
     title =  models.CharField(max_length=120)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=100, default=29.99)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=100,
+                                     blank=True, null=True)
     slug = models.SlugField()
     timestamp = models.DateField(auto_now_add=True, auto_now=False)
     updated = models.DateField(auto_now_add=False, auto_now=True)
@@ -11,3 +13,14 @@ class Product(models.Model):
     
     def __str__(self):
         return self.title
+
+class ProductImamge(models.Model):
+    product = models.ForeignKey(Product)
+    image = models.FileField(upload_to='products/images/', null=True)
+    featured = models.BooleanField(default=False)
+    thumbnail = models.BooleanField(default=False)
+    updated = models.DateField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product.title
