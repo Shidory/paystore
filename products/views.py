43313d351fb1934ec6f05eaad55bee380ca2246c
Simon MWEPU <simonmwepu@gmail.com>
2019-06-15 07:47:30 +0200

    [ADD] views

--- products/views.py
+++ products/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def home(request):
-    pass
+    if request.user.is_authenticated():
+        username = "Shidory"
+        context = {"username": request.user}
+    else:
+        context = {"username": request.user}
+    template = 'products/home.html'
+    return render(request, template, context)
+
+
+def all(request):
+    context = {"products": Product.objects.all()}
+    template = 'products/all.html'
+    return render(request, template, context)
