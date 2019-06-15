750ff75688201215fc16b4a93cfdeefeb39a9a80
Simon MWEPU <simonmwepu@gmail.com>
2019-06-15 14:44:34 +0200

    [ADD] ProductAdmin class

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Updated
+    date_hierarchy = 'timestamp'
+    # Can search now
+    search_fields = ['price', 'description']
+    list_display = ['title', 'price', 'active', 'updated']
+    list_editable = ['price', 'active']
+    list_filter = ['price', 'active']
+    #readonly_fields = ['update', 'active']
+    #prepopulated_fields = {"slug": ("title,")}
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
