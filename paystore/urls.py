4bf6857404122b60018823072fc685326e4204ea
Simon MWEPU <simonmwepu@gmail.com>
2019-06-15 14:41:01 +0200

    [ADD] a autodiscover

--- paystore/urls.py
+++ paystore/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 from . import views
+admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
