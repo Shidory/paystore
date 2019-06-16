f18a53f32041aa7c29bd4c7d7617311913fb368a
Simon MWEPU <simonmwepu@gmail.com>
2019-06-16 20:17:13 +0200

    [ADD] Some urlpatterns

--- paystore/urls.py
+++ paystore/urls.py
@@ -13,14 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+# Configuration du service static
+
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
-    path('', views.home, name='home'),
+    #path('', include('products.urls')),
+    path('', views.index, name="index"),
 ]
+
+# A Utiliser si DEBUG est activé
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
