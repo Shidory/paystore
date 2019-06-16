10c7493d3908017208c0d248514abf5f548bc435
Simon MWEPU <simonmwepu@gmail.com>
2019-06-16 20:17:55 +0200

    [UPDATE] index

--- paystore/views.py
+++ paystore/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 
-def home(request):
-    if request.user.is_authenticated():
-        username = "Shidory"
-        context = {"username": request.user}
-    else:
-        context = {"username": request.user}
-        
-    template = 'products/home.html'
-    context = locals()
-    return render(request, template, context)
+def index(request):
+    template = 'index.html'
+    return render(request, template)
