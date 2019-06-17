d153c627eb6e8a18941d9e4de06577172d3ee339
Simon MWEPU <simonmwepu@gmail.com>
2019-06-17 16:25:32 +0200

    [UPDATE] Unique slug

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('all', views.all, name='all'),
     path('', views.home, name='home'),
+    path('(?P<id>\+d)/', views.single, name='single_product'),
 ]
