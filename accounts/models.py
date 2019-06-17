264038e9583374a369024b59e388cc68c2fac467
Simon MWEPU <simonmwepu@gmail.com>
2019-06-17 16:38:12 +0200

    [ADD] UserLogin and UserSignUp

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class UserLogin(models.Model):
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.email
+
+class UserSignUp(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
