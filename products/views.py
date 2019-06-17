63700c1bc2691163d85f8a826e5aa3f0ccbcaf0c
Simon MWEPU <simonmwepu@gmail.com>
2019-06-17 16:26:15 +0200

    [UPDATE] Try to fix bug

--- products/views.py
+++ products/views.py
@@ -14,3 +14,14 @@ def all(request):
     context = {"products": products}
     template = 'products/all.html'
     return render(request, template, context)
+
+def single(request, id):
+    try:
+        product = Product.objects.get(id=id)
+        print(product.title)
+        products = Product.objects.all()
+        context = {"products": products}
+        template = 'products/all.html'
+        return render(request, template, context)
+    except:
+        pass
