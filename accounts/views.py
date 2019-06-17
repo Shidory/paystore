19ba374e2d35c0db0c1b0615be8ead228e72d4f0
Simon MWEPU <simonmwepu@gmail.com>
2019-06-17 10:34:33 +0200

    [ADD] login and logout method

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import logout, login, authenticate
 
-# Create your views here.
+def logout_view(request):
+    print("logging out")
+    logout(request)
+    return HttpResponseRedirect("/")
+
+def login_view():
+    form = LoginForm(request.POST or None)
