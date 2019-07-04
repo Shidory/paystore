d98a9656600d429f4952f3c4704301b036f7322e
Simon MWEPU <simonmwepu@gmail.com>
2019-07-04 22:06:46 +0200

    [UPDATE] login, logout, register path

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name="login"),
+    path('login/', views.login_view, name="auth_login"),
+    path('logout/', views.logout_view, name="auth_logout"),
+    path('register/', views.registration_view, name="auth_register"),
 ]
