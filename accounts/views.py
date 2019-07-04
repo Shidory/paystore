87e214be298a6c9305a944370681ffc40aa9e2e8
Simon MWEPU <simonmwepu@gmail.com>
2019-07-04 22:05:51 +0200

    [ADD] Checking registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,36 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth import logout, login, authenticate
-from django.contrib import auth
-from django.contrib.auth.models import User
-from django.contrib import auth
-from django.core.exceptions import ObjectDoesNotExist
-
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        try:
-            user = auth.authenticate(username=username, password=password)
-            if user is not None:
-                auth.login(request, user)
-                return render(request, 'home.html')
-            else:
-                messages.error(request, 'Username and password did not matched!')
-        except auth.ObjectNotExist():
-            print("Invalid user")
-
-    return render(request, 'login.html')
-
+from .forms import LoginForm, RegistrationForm
 
 def logout_view(request):
     print("logging out")
     logout(request)
     return HttpResponseRedirect("/")
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        user = authenticate(username=username, password=password)
+        login(request, user)
+    context = {
+        "form": form
+    }
+    return render(request, "form.html", context)
+
+def registration_view(request):
+    form = RegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        # new_user.first_name = "simon"
+        new_user.save()
+        # username = form.cleaned_data["username"]
+        # password = form.cleaned_data["password"]
+        # user = authenticate(username=username, password=password)
+        # login(request, user)
+    context = {
+        "form": form
+    }
+    return render(request, "form.html", context)
 
