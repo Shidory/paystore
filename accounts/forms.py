95d35243093f09c4e0a4305cfd764fc14082517e
Simon MWEPU <simonmwepu@gmail.com>
2019-07-04 22:08:49 +0200

    [UPDATE] Changing attribs

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,10 @@ User = get_user_model()
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Username'}
+        attrs={'class': 'form-control'}
     ))
     password = forms.CharField(widget=forms.PasswordInput(
-        attrs={'class': 'form-control', 'placeholder': 'Password'}
+        attrs={'class': 'form-control'}
     ))
 
     def clean_username(self):
@@ -35,8 +35,38 @@ class LoginForm(forms.Form):
             return password
 
 class RegistrationForm(forms.ModelForm):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Password'}
+    ))
+    passwordconf = forms.CharField(label='Password confirmation ', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Password confirmation'}
+    ))
     class Meta:
         model = User
+        fields = ['username', 'email']
+
+    def clean_passwordconf(self):
+        password = self.cleaned_data.get("password")
+        passwordconf = self.cleaned_data.get("passwordconf")
+        if password and passwordconf and password != passwordconf:
+            raise forms.ValidationError("Passwords do not match")
+        return passwordconf
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        user_count = User.objects.filter(email=email).count()
+        if user_count > 0:
+            raise forms.ValidationError("This email has already been\
+                                     registered. Please reset your password.")
+        return email
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 """from .models import *
 from django.core.validators import validate_email
