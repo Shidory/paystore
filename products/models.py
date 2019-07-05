356d3a8767b6f21da08aa5fc38fbe80b726ed974
Simon MWEPU <simonmwepu@gmail.com>
2019-07-05 09:54:19 +0200

    [UPDATE] Product's model

--- products/models.py
+++ products/models.py
@@ -23,7 +23,6 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("single_product", kwargs={"slug": self.slug})
-        pass
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
